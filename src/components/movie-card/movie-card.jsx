ac78515c11ce73141977bec323f54c1631f8cf07
Doug Hill <stix2you@gmail.com>
2024-03-01 11:50:43 -0600

    install bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,19 @@
 import PropTypes from "prop-types";
+import { Card, Button } from "react-bootstrap";
+import "./movie-card.scss";
 
-export const MovieCard = ({ movie, onMovieClick }) => {    // create a functional component called MovieCard, which takes two props: movie and onMovieClick
-    return (                           // returns a new piece of UI
-        <div className="movie-card"           // style={{ textAlign: 'center' }}
-            onClick={() => {           // when the div is clicked, it will call onMovieClick with the movie as an argument
-                onMovieClick(movie);     // onMovieClick is a prop that's passed to the MovieCard component
-            }}
-        >
-            <h2>{movie.title}</h2>         
-        </div>                // returns the title of the movie
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (                // The Card component is wrapped in a div with the class name "d-flex flex-row" in order to stack image beside the title and description.                                   
+        <Card style={{ width: '15rem' }} onClick={() => onMovieClick(movie)} className="d-flex flex-row">             
+            <Card.Img variant="top" src={movie.image} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.author}</Card.Text>
+                <Button onClick={() => onMovieClick(movie)} variant="link">
+                    Open
+                </Button>
+            </Card.Body>
+        </Card>
     );
 };
 
